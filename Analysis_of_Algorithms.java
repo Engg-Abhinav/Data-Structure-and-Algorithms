e3ee8d4ed6c547d8da53d428033d190325e12656
Engg-Abhinav <104521563+Engg-Abhinav@users.noreply.github.com>
2023-04-05 20:43:51 +0530

    Analysis of Algorithm

--- Analysis_of_Algorithms.java
+++ Analysis_of_Algorithms.java
@@ -1,1 +1,19 @@
-!
+import java.util.*;
+
+// Given a number n, write a function to find the sum of first n natural numbers
+
+public class Analysis_of_Algorithms {
+	int i;
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of n: ");
+		int n = sc.nextInt();
+		int i, sum = 0;
+		
+		for(i=0;i<=n;i++) {
+			sum = sum + i;
+		}
+		
+		System.out.println("Sum of " + n + " numbers is: " + sum);
+	}
+}
